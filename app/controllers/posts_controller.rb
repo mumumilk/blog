6aa7aa3f76b5501aaf6a302951bcee7f9f6e3777
Henrique Reche <henrique_nacamoto@hotmail.com>
2017-10-04 22:10:45 -0300

    Creating delete and update actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,36 @@ class PostsController < ApplicationController
     def new
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
     def create
         @post = Post.new(post_params)
         
         @post.save
         redirect_to @post
     end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
     
     def show
         @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        redirect_to '/'
     end
 
     private
